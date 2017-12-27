a948e83265d4898c92dd9f4faf9d5bc6e072662a
Shannon Nolan <shannonnolan17@gmail.com>
2017-12-27 18:38:54 -0500

    Add carousel to app

--- rooster_grin_front_end_test/src/App.js
+++ rooster_grin_front_end_test/src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { Carousel } from 'react-responsive-carousel';
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
+      <div>
+        <Carousel autoPlay={true} showArrows={true} showThumbs={false} dynamicHeight={true} infiniteLoop={true}>
+          <div>
+            <img src={require('./carousel_images/hero-img.jpg')} />
+          </div>
+          <div>
+            <img src={require('./carousel_images/iStock-503300108.jpg')} />
+          </div>
+        </Carousel>
       </div>
     );
   }
