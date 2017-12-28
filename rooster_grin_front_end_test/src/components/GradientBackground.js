7a4c3383a686f2c2fc5b20db3d7f7736c5d39130
Shannon Nolan <shannonnolan17@gmail.com>
2017-12-28 15:59:00 -0500

    Add roboto font

--- rooster_grin_front_end_test/src/components/GradientBackground.js
+++ rooster_grin_front_end_test/src/components/GradientBackground.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react';
 class GradientBackground extends Component {
   render() {
     return (
-      <div>
+      <div id="box-search">
 
-        <div className="thumbnail text-center">
-          <img src={require('../images/slanted-gradient-background.svg')} />
-          <div className="block-center caption post-content">
-            <h3>A great smile is one of life's greatest assets.  Everyone deserves a smile that they can be proud of.  At Anne O'Day Orthodontics we will listen to you and guide you to the smile of your dreams!</h3>
+        <div className="thumbnail" >
+          <img className="gradient-background img-responsive" src={require('../images/slanted-gradient-background.svg')} />
+          <div className="caption">
+            <h3 className="gradient-text ">A great smile is one of life's greatest assets.  Everyone deserves a smile that they can be proud of.  At Anne O'Day Orthodontics we will listen to you and guide you to the smile of your dreams!</h3>
           </div>
         </div>
 
