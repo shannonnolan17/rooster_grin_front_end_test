7e19059af4e65969e4092187ac202b6586c8974d
Shannon Nolan <shannonnolan17@gmail.com>
2017-12-31 14:04:30 -0500

    Add social media icons to footer

--- rooster_grin_front_end_test/src/components/Footer.js
+++ rooster_grin_front_end_test/src/components/Footer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Form } from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome'
+
 
 class Footer extends Component {
   render() {
@@ -26,23 +28,15 @@ class Footer extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4 footer-links">
-              <h5 className="title2">Links</h5>
-              <ul>
-                <li className="footer-paragraph"><a href="#!">Link 1</a></li>
-                <li className="footer-paragraph"><a href="#!">Link 2</a></li>
-                <li className="footer-paragraph"><a href="#!">Link 3</a></li>
-                <li className="footer-paragraph"><a href="#!">Link 4</a></li>
-              </ul>
+              <p>We offer the most effective and popular techniques including Traditional braces, clear ceramic braces, certified Invisalign preferred provider, fun-colored elastics, mouthguards, colored retainers, orthognathic surgery, interdisciplinary cases, state-of-the-art CT imaging, digital X-rays and photos and digital models eliminate taking impressions.</p>
             </div>
 
             <div className="col-md-4 footer-links">
-              <h5 className="title2">Links</h5>
-              <ul>
-                <li className="footer-paragraph"><a href="#!">Link 1</a></li>
-                <li className="footer-paragraph"><a href="#!">Link 2</a></li>
-                <li className="footer-paragraph"><a href="#!">Link 3</a></li>
-                <li className="footer-paragraph"><a href="#!">Link 4</a></li>
-              </ul>
+              <h5 className="title2 justify-text-center">Get in Touch</h5>
+               <a href="#" className="fa fa-facebook"></a>
+               <a href="#" className="fa fa-twitter"></a>
+               <a href="#" className="fa fa-instagram"></a>
+               <a href="#" className="fa fa-youtube"></a>
             </div>
 
 
